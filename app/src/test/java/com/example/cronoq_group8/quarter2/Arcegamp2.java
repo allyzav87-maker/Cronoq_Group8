455b8bd8a3840058ec65ff2910a1c5b48edc4859
My PC <micoxd81@gmail.com>
2026-09-17 15:17:59 +0800

    Added Arcega profile test

---
+++ app/src/test/java/com/example/cronoq_group8/quarter2/Arcegamp2.java
@@ -0,0 +1,22 @@
+package com.example.cronoq_group8.quarter2;
+
+import org.junit.Test;
+
+public class Arcegamp2 {
+    @Test
+    public void printMyprofile() {
+
+        // --- 1. THE INPUT (Storing your personal details in variables) ---
+        String myname = "miguel arcega";
+        String petName = "koko";
+        String favFood = "sinigang";
+        int myage = 17;
+
+        // --- 2. THE OUTPUT (Printing to the console) ---
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myname + " and I am " + myage + " years old.");
+        System.out.println("I have wonderful pets named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day!");
+    }
+}
+
