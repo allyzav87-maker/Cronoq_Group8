d90b4ae999296a530a86ba0b3396c8fee376805a
SHS PC <lawrencebagaslaocarian04@gmail.com>
2026-09-18 08:42:42 +0800

    Added Ladrera profile test

--- app/src/test/java/com/example/cronoq_group8/quarter2/Ladreramp2.java
+++ app/src/test/java/com/example/cronoq_group8/quarter2/Ladreramp2.java
@@ -11,9 +11,9 @@ public class Ladreramp2  {
         int myAge = 17;
 
         System.out.println("--- MY DIGITAL PROFILE ---");
-        System.out.println("Hello, my name is" + myName + " and I am " + myAge + "years old.");
-        System.out.println("I have a wonderful pet named" + petName + ".");
-        System.out.println("If I could, I would eat" + favFood + "every single day");
+        System.out.println("Hello, my name is " + myName + " and I am " + myAge + " years old.");
+        System.out.println("I have a wonderful pet named " + petName + ".");
+        System.out.println("If I could, I would eat " + favFood + " every single day.");
 
     }
 }
